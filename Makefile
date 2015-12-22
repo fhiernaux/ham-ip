13aae0d0f1794eaee3c2a1d5a01d90dc863f1619
Sebastien Van Cauwenberghe <svancau@gmail.com>
2015-12-22 15:50:12 +0100

    Add autotools support

--- Makefile
+++
@@ -1,6 +0,0 @@
-all:
-	g++ -Wall -o ham-ip src/*.cpp
-
-clean:
-	rm -f src/*.o ham-ip
-
--- runme.sh
+++ runme.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-sudo ./ham-ip &
+sudo ./hamip &
 sudo ip link set ham0 up
 sudo ip addr add 10.0.0.1/24 dev ham0
 
